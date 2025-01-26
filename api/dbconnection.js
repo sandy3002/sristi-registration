08c79a794487dd23fc23dd393f6456fbdd61c238
Sampurno Sarkar <sampurnosarkar.ind@gmail.com>
2025-01-27 01:15:31 +0530

    testing running without vercel

--- api/dbconnection.js
+++ api/dbconnection.js
@@ -1,15 +1,15 @@
-require('dotenv').config(); // Load .env variables
-
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`database connected: ${connect.connection.host}`);
+    const connect = await mongoose.connect(`mongodb+srv://sampurnosarkarind:wX00hEY8ky3w4NR2@sristidb.3qk5b.mongodb.net/?retryWrites=true&w=majority&appName=sristidb`);
+    console.log(
+      `database connected: ${connect.connection.host}`
+    );
   } catch (error) {
     console.log("error", error);
     process.exit(1);
   }
 };
 
-module.exports = dbConnect;
+module.exports = dbConnect;
